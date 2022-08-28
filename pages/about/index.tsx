0bc1c41d83bcae6dcbc2d60954af31eee51c54ca
seymenklc <seymennklc@gmail.com>
2022-08-28 13:16:30 +0300

    edited about page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,10 +1,21 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import PageHead from "../../components/PageHead";
 
 export default function About() {
+    const router = useRouter();
+
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/');
+        }, 3000);
+    }, [router]);
+
     return (
         <div>
             <PageHead title='About' />
-            <h1>About Page</h1>
+            <h3>Still working on it...</h3>
+            <p>Maybe visit sometime later?</p>
         </div>
     );
 }
