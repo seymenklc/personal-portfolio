b1d7db07164a113890f630917ec47aca6a90c3f6
seymennklc <seymennklc@gmail.com>
2022-11-20 15:58:20 +0300

    added bio

--- config.ts
+++ config.ts
@@ -4,19 +4,17 @@ export default {
     surname: 'Kılıç',
     jobTitle: 'Web Developer',
     description: `A Developer based on Turkey.
-    At day, a Front-End developer at night, a Back-End developer.
-    Always eager to learn something new, but at the same time lazy.
-    Looks at all the possibilities and chooses the best one 
-    for the situation. And one last thing; his glasses are cool.`,
-    about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-    Quidem ipsum blanditiis sequi culpa excepturi. Minus saepe fugit blanditiis nostrum. 
-    Repellat laudantium inventore laboriosam quos! Libero nemo mollitia 
-    recusandae dolor natus sint quae eos assumenda? Quasi adipisci cumque labore molestias 
-    voluptatum aspernatur quidem ducimus officiis itaque nam libero, voluptatem, debitis cum 
-    fuga ad rerum, dolorum saepe at sunt placeat modi quam? Ipsum accusamus autem, architecto 
-    dolore iusto dicta veritatis rerum facilis voluptas odit possimus. Illum magnam sunt impedit
-    suscipit atque excepturi nostrum quam architecto iste maiores animi accusantium, deleniti 
-    rem accusamus. Hic ea rerum repellendus quam maxime ullam, iste molestiae pariatur.`,
+    At day, Front-End, at night, a Back-End developer.
+    Always eager to learn something new and passionate.
+    Evaluates all the possibilities and chooses the best one 
+    for the situation. One last thing; his glasses are cool.`,
+    about: `As a self-taught developer, I've spent all of my spare time working on
+    Javascript and React in the recent 3+ years. As a result of studying
+    electronics during my high school period, it gave me a different
+    perspective about how electronic devices work including computers
+    in large amounts of detail, and that affected my developer journey
+    tremendously. At the moment, I'm continuing to improve my tech
+    skills every day while studying for my major.`,
     contact: {
         mail: 'seymennklc@gmail.com',
         links: {
